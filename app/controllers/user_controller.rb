18c23d6278102319fa18e29c5e28d6a44668aa60
49497974m <49497974tkhsm>
2019-06-12 18:33:31 +0900

    Update user_controller.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,10 @@
 class UserController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
