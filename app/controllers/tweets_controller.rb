2b51b2f569347b7e7005223948e4751276704d8a
49497974m <49497974tkhsm>
2019-06-12 17:44:35 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,15 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    
+  end
 
   def index
+    @tweet = Tweet.all
   end
   
 end
