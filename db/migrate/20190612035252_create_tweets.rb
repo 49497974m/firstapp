3dc8a00915c440429cfe328934d0fff23565cabf
49497974m <49497974tkhsm>
2019-06-12 15:12:40 +0900

    Update 20190612035252_create_tweets.rb

--- db/migrate/20190612035252_create_tweets.rb
+++ db/migrate/20190612035252_create_tweets.rb
@@ -1,8 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-
-      t.timestamps
+      t.text      :text
+      t.string    :user_id
+      t.timestamps null: true
     end
   end
 end
