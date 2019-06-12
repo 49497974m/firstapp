0cefb5e6a4e573d2957ee324f98203c9896c4aa6
49497974m <49497974tkhsm>
2019-06-12 20:51:21 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,24 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+
   def new
     @tweet = Tweet.new
   end
 
   def create
-    
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweet_path
   end
 
   def index
-    @tweet = Tweet.all
+    @tweet = Tweet.order("create_at DESC")
   end
   
+  private
+    def tweet_params
+      params.require(:tweet).permit(:text)
+    end
+
 end
