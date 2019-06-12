409416fcec09e804a2c4529a232d0948fb3ce523
49497974m <49497974tkhsm>
2019-06-12 17:51:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/show'
+
   devise_for :users
   root to: 'tweets#index'
-  get 'tweets' => 'tweets#index'
+  resources :tweets
+  resources :users
+  
 end
