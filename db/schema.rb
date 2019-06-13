990d9251eba1a541c3cba02451cba2346aeb2091
49497974m <49497974tkhsm>
2019-06-13 21:07:03 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_081329) do
+ActiveRecord::Schema.define(version: 2019_06_13_064434) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
-    t.string "user_id"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
